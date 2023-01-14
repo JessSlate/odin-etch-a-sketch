6cadec8daabfc99ff9be941af3fd7fd11a7d4243
Jess Slate <jessmslate@gmail.com>
2023-01-14 18:50:33 -0500

    Add styling to visualize divs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+const GRID_SIZE = 16;
 // generate a 16x16 grid of divs
 // and put them inside the 'container' div
 const canvasContainer = document.getElementById("canvas-container");
 //create 16 x 16 div grid
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
+for (let i = 0; i < GRID_SIZE; i++){
+    for (let j = 0; j < GRID_SIZE; j++){
         canvasContainer.appendChild(document.createElement("div"));
     };
 };
