e54481381a8f0fb7435d92099f106e05c7f8921e
Jess Slate <jessmslate@gmail.com>
2023-01-15 14:50:19 -0500

    Change canvas size with user input

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-const GRID_SIZE = 16;
-// generate a 16x16 grid of divs
-// and put them inside the 'container' div
-const canvasContainer = document.getElementById("canvas-container");
-//create 16 x 16 div grid
-for (let i = 0; i < GRID_SIZE; i++){
-    for (let j = 0; j < GRID_SIZE; j++){
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("pixel");
-        canvasContainer.appendChild(newDiv);
+
+function createPixelCanvas(gridSize = 16){
+    // generate a grid of divs
+    // and put them inside the 'container' div
+    const canvasContainer = document.getElementById("canvas-container");
+
+    //clear out any previously generated divs
+    canvasContainer.textContent = "";
+    
+    //create div grid
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++){
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("pixel");
+            canvasContainer.appendChild(newDiv);
+        };
     };
 };
 
 function fillPixel(e){
     pixel = e.target;
 
-    console.log(pixel);
-    if (pixel.classList != "pixel") return;
+    //ignore non-pixel elements 
+    if (pixel.classList != "pixel") return; 
+
     pixel.classList.add("filled");
-    
-}
+};
 
 function reset(){
     let pixelCanvas = document.getElementsByClassName("pixel");
+    //remove the 'filled' class
     for (let pixel of pixelCanvas){
         pixel.classList.remove("filled");
-    }
+    };
+    createPixelCanvas(getGridSize());
+};
+
+function getGridSize(){
+    let userSize = prompt("Hello");
+    return userSize;
 }
+
+createPixelCanvas();
 
 window.addEventListener('mouseover', fillPixel);
 const resetBtn = document.getElementById("reset-button");
 resetBtn.addEventListener('click', reset);
-//newDiv.textContent = '\xa0';
